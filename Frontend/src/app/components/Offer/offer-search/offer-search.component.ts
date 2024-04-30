91ae78b93f3fe5a544f4054661e493a5f936a5d1
Wiktoria Lewicka <wiktorialewicka258@gmail.com>
2024-04-30 14:16:58 +0200

    Add offer - project structure only

---
+++ Frontend/src/app/components/Offer/offer-search/offer-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-search.component.html',
+  styleUrl: './offer-search.component.css'
+})
+export class OfferSearchComponent {
+
+}
---
+++ Frontend/src/app/components/Offer/single-offer/single-offer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-offer',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-offer.component.html',
+  styleUrl: './single-offer.component.css'
+})
+export class SingleOfferComponent {
+
+}
