8a4f8ef2b303fbbea0cef165298a7e3b396697a9
Wiktoria Lewicka <wiktorialewicka258@gmail.com>
2024-05-15 20:32:53 +0200

    Update getFlightByDate

--- TransportService/src/main/java/com/example/transportation/flight/repo/FlightRepository.java
+++ TransportService/src/main/java/com/example/transportation/flight/repo/FlightRepository.java
@@ -2,14 +2,18 @@ package com.example.transportation.flight.repo;
 
 import com.example.transportation.flight.domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, UUID> {
-    List<Flight> findAllByDepartureDate(String date);
+    @Query("SELECT f FROM Flight f WHERE DATE(f.departureDate) = DATE(:date)")
+    List<Flight> findAllByDepartureDateIgnoringTime(@Param("date") Date date);
 
     Flight findByPrice(int price);
 }
