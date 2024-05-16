fe4ca09a89c9457ca0ebdb33a16dc245ab22c48d
Wiktoria Lewicka <wiktorialewicka258@gmail.com>
2024-05-16 15:31:20 +0200

    Quick Flight Fix

--- APIGateway/src/main/java/com/rsww/mikolekn/APIGateway/offer/model/OfferInfoModel.java
+++ APIGateway/src/main/java/com/rsww/mikolekn/APIGateway/offer/model/OfferInfoModel.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Setter
 @Builder
 public class OfferInfoModel {
-
+    @JsonProperty("hotel_name")
     private String hotelName;
-
+    @JsonProperty("country")
     private String country;
-
+    @JsonProperty("stars")
     private int stars;
-
+    @JsonProperty("rooms")
     private List<RoomInOfferModel> rooms;
-
+    @JsonProperty("available_flights_to")
     private List<Flight> availableFlightsTo;
-
+    @JsonProperty("available_flights_from")
     private List<Flight> availableFlightsFrom;
 
     @Builder
@@ -29,7 +29,9 @@ public class OfferInfoModel {
     @Getter
     @AllArgsConstructor
     public static class RoomInOfferModel {
+        @JsonProperty("type")
         private String type;
+        @JsonProperty("price")
         private float price;
     }
 }
