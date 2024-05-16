54cbb37f6f9b1928e4f89327e8a96c64b0fe1d51
Bartłomiej Szczepaniec <104697469+bartSzczepaniec@users.noreply.github.com>
2024-05-16 23:16:29 +0200

    GetAllOrders and TripService mongo fixed

--- APIGateway/src/main/java/com/rsww/mikolekn/APIGateway/order/GetAllOrdersResponse.java
+++ APIGateway/src/main/java/com/rsww/mikolekn/APIGateway/order/GetAllOrdersResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class GetAllOrdersResponse extends AbstractResponse {
--- TripService/src/main/java/com/rsww/lydka/TripService/listener/events/orders/GetAllOrdersRequest.java
+++ TripService/src/main/java/com/rsww/lydka/TripService/listener/events/orders/GetAllOrdersRequest.java
@@ -1,13 +1,11 @@
 package com.rsww.lydka.TripService.listener.events.orders;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class GetAllOrdersRequest {
