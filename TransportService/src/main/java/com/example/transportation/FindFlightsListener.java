611a511496c2151878d40a6fb67165c167de4b87
dlydka <lydkadm@gmail.com>
2024-06-13 17:02:07 +0200

    added flight info to preferences

--- TransportService/src/main/java/com/example/transportation/FindFlightsListener.java
+++ TransportService/src/main/java/com/example/transportation/FindFlightsListener.java
@@ -3,7 +3,11 @@ package com.example.transportation;
 import com.example.transportation.DTO.GetFlightInfoRequest;
 import com.example.transportation.DTO.GetFlightsInfoResponse;
 import com.example.transportation.flight.domain.Flight;
+import com.example.transportation.flight.domain.FlightReservation;
 import com.example.transportation.flight.repo.FlightRepository;
+import com.example.transportation.flight.repo.FlightReservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,14 +17,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class FindFlightsListener {
+    private final Logger logger = LoggerFactory.getLogger(FindFlightsListener.class);
     private final FlightRepository repository;
+    private final FlightReservationRepository reservationRepository;
 
     @Autowired
-    public FindFlightsListener(FlightRepository repository) {
+    public FindFlightsListener(FlightRepository repository, FlightReservationRepository reservationRepository) {
         this.repository = repository;
+        this.reservationRepository = reservationRepository;
     }
 
     @RabbitListener(queues = "${spring.rabbitmq.queue.findFlightQueue}")
@@ -30,5 +39,18 @@ public class FindFlightsListener {
 
         List<Flight> flights = repository.findAllByDepartureDate(date);
         return new GetFlightsInfoResponse(flights);
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue.GetFlightQueue}")
+    public Flight getFlight(String flightId) {
+        Optional<FlightReservation> optionalFlightReservation = reservationRepository.findById(UUID.fromString(flightId));
+        if (optionalFlightReservation.isEmpty()) {
+           return null;
+        }
+        FlightReservation reservation = optionalFlightReservation.get();
+
+        Flight flight = reservation.getFlight();
+        logger.info("Flight: " + flight);
+        return flight;
     }
 }
